3b817ae86a1574fbafb242bafd8128b6cbab309f
AlirezaTashani <alirezatd80@gmail.com>
2025-12-21 20:09:31 +0330

    handle error for location

--- app/handlers.py
+++ app/handlers.py
@@ -1,4 +1,4 @@
-from telethon import events
+from telethon import events , Button
 from  app import client
 from app.text import welcome_text
 from app.buttons import submit_location_inline , main_menu , change_location
@@ -7,16 +7,22 @@ from app.utils import find_name_city
 
 @client.on(events.NewMessage(pattern='/start'))
 async def start(event: Message):
-    await event.respond(welcome_text , buttons=submit_location_inline())
+   await event.respond(welcome_text , buttons=submit_location_inline())
 
 @client.on(events.CallbackQuery(data='submit_location'))
 async def submit_location(event:events.CallbackQuery.Event):
+    await event.edit(welcome_text)
     await event.respond("لطفاً موقعیت مکانی خود را ارسال کنید.", buttons=change_location())
     
 @client.on(events.NewMessage())
 async def handle_location(event:Message):
     if (event.message.geo):
-        lat = event.message.geo.lat
-        lon = event.message.geo.long
-        city_name = find_name_city(lat,lon)
-        await event.respond(f"مکان شما : {city_name}")
+        try:
+            lat = event.message.geo.lat
+            lon = event.message.geo.long
+            city_name = find_name_city(lat,lon)
+            await event.delete()
+            await event.respond(f"مکان شما : {city_name}",buttons=Button.clear())
+        except:
+            await event.delete()
+            await event.respond("خطا در پیدا کردن نام شهر. لطفاً دوباره تلاش کنید.",buttons=change_location())
